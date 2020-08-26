d7e5a4e3c2ac2235775a624d64483771fd4a167d
Juuso Takalainen <juuso@streamr.network>
2020-08-26 12:03:00 +0300

    Clean up MockAMB
    remove compile warnings

--- build/flattened/MockAMB.sol
+++ build/flattened/MockAMB.sol
@@ -3,20 +3,12 @@
 
 pragma solidity ^0.6.0;
 
-interface IAMB {
+contract MockAMB {
     function requireToPassMessage(
         address _contract,
-        bytes calldata _data,
-        uint256 _gas
-    ) external returns (bytes32);
-}
-
-contract MockAMB is IAMB {
-    function requireToPassMessage(
-        address _contract,
-        bytes memory _data,
-        uint256 _gas
-    ) override public returns (bytes32) {
+        bytes memory, // _data,
+        uint256 // _gas
+    ) public returns (bytes32) {
         return bytes32(bytes20(_contract));
     }
 }
