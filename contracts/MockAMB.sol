0928a757665bfb27dd1bffb516edf09bfcdbb3c7
Juuso Takalainen <juuso@streamr.network>
2020-09-05 13:54:58 +0300

    Try to debug why eth flow test fails
    apparently initialization reverts somehow; however, when I do it with same params(?) from JS, seems to work. Hmm.

--- contracts/MockAMB.sol
+++ contracts/MockAMB.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 contract MockAMB {
     address public messageSender;
+    event ContractCallSuccess(bool wasSuccess);
     /*
     call on LOCAL blockchain. no bridge
     */
@@ -12,6 +13,7 @@ contract MockAMB {
     ) public returns (bytes32) {
         messageSender = msg.sender;
         (bool success, ) = _contract.call{gas: _gas}(_data);
+        emit ContractCallSuccess(success);
         messageSender = address(0);
         return bytes32(byte(success ? 0x1 : 0x0));
     }
