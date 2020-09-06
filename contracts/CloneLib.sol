0475a36831a889a198277855ca43ea03a3595c36
Isentropy <jwolff@isentropy.com>
2020-09-06 23:12:49 +0200

    Update CloneLib.sol
    added 0x0 address check to deployCodeAndInitUsingCreate for consistency

--- contracts/CloneLib.sol
+++ contracts/CloneLib.sol
@@ -76,6 +76,7 @@ library CloneLib {
         assembly {
             proxy := create(0, add(code, 0x20), len)
         }
+        require(proxy != address(0), "error_create");
         if (initData.length > 0) {
             (bool success, ) = proxy.call(initData);
             require(success, "error_initialization");
