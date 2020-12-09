3766bc134d65fe9e5b0aa68ad09296b0970f29d8
Jonathan Wolff <jwolff@isentropy.com>
2020-12-09 12:45:47 +0100

    clarified comment

--- build/flattened/DataUnionMainnet.sol
+++ build/flattened/DataUnionMainnet.sol
@@ -926,15 +926,15 @@ pragma solidity 0.6.6;
 interface ITokenMediator {
     function bridgeContract() external view returns (address);
 
-    //from is msg.sender
-    // multi-token bridge mediator uses this method
+    // The new mediator contracts relayTokens() have no from arg and always relay from msg.sender
+    // multi-token mediator uses this method
     function relayTokens(address erc20, address _receiver, uint256 _value) external;
-    // single-token bridge mediator uses this method
+    // single-token mediator uses this method
     function relayTokens(address _receiver, uint256 _value) external;
     
     //returns:
-    //MultiAMB 0xb1516c26 == bytes4(keccak256(abi.encodePacked("multi-erc-to-erc-amb")))
-    //Single token AMB 0x76595b56 ==  bytes4(keccak256(abi.encodePacked("erc-to-erc-amb")))
+    //Multi-token mediator: 0xb1516c26 == bytes4(keccak256(abi.encodePacked("multi-erc-to-erc-amb")))
+    //Single-token mediator: 0x76595b56 ==  bytes4(keccak256(abi.encodePacked("erc-to-erc-amb")))
     function getBridgeMode() external pure returns (bytes4 _data);
 
 }
