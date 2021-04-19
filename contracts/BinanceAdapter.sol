dc69ec7a7475f79c079526fdad1b6f4a909d7fdb
Jonathan Wolff <jwolff@isentropy.com>
2021-04-19 04:22:51 +0200

    lint fix

--- contracts/BinanceAdapter.sol
+++ contracts/BinanceAdapter.sol
@@ -10,6 +10,7 @@ contract BinanceAdapter {
     address public bscBridge;
     IERC677 public dataCoin;
     address public convertToCoin;
+
     /*
     ERC677 callback
     */
@@ -39,7 +40,6 @@ contract BinanceAdapter {
             require(sendToBinanceAmount >= amountDatacoin, "insufficient_balance");
         }
         else{
-            //require(dataCoin.approve(address(honeyswapRouter), 0), "approve_failed");
             require(dataCoin.approve(address(honeyswapRouter), amountDatacoin), "approve_failed");
             address[] memory path = new address[](2);
             path[0] = address(dataCoin);
@@ -67,15 +67,16 @@ contract BinanceAdapter {
             b := m
         }
     }
+
     //from https://github.com/GNSPS/solidity-bytes-utils/blob/6458fb2780a3092bc756e737f246be1de6d3d362/contracts/BytesLib.sol#L297
     function toAddress(bytes memory _bytes) internal pure returns (address) {
         require(_bytes.length >= 20, "toAddress_outOfBounds");
         address tempAddress;
+        // solhint-disable-next-line no-inline-assembly
         assembly {
             tempAddress := div(mload(add(_bytes, 0x20)), 0x1000000000000000000000000)
         }
 
         return tempAddress;
     }
-
 }
