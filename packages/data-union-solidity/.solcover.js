16e700f51db56a6d56033bc9aae1381ab4b13c10
Juuso Takalainen <juuso@streamr.network>
2021-09-22 15:00:59 +0300

    fix: syntax for solcover ignore

--- packages/data-union-solidity/.solcover.js
+++ packages/data-union-solidity/.solcover.js
@@ -3,7 +3,7 @@ module.exports = {
     // right now these must be skipped because they can't be compiled with optimizations off because of the huge amount of arguments in the initialize function
     // CompilerError: Stack too deep when compiling inline assembly: Variable headStart is 1 slot(s) too deep inside the stack.
     skipFiles: [
-        "contracts/DataUnionMainnet.sol",
-        "contracts/DataUnionFactoryMainnet.sol",
+        "DataUnionMainnet.sol",
+        "DataUnionFactoryMainnet.sol",
     ]
 }
