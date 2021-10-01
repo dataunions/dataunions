b27b2d2b4c7f5f58887f72432911728b31ae646b
Juuso Takalainen <juuso@streamr.network>
2021-10-01 12:38:37 +0300

    fix: cleanup
    remove copy-pasted unused import, add newline to end of file

--- packages/data-union-solidity/contracts/IJoinListener.sol
+++ packages/data-union-solidity/contracts/IJoinListener.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.6;
 
-import "./IERC677.sol";
-
 interface IJoinListener {
     function onJoin(address newMember) external;
 }
--- packages/data-union-solidity/contracts/IPartListener.sol
+++ packages/data-union-solidity/contracts/IPartListener.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.6;
 
-import "./IERC677.sol";
-
 interface IPartListener {
     function onPart(address leavingMember) external;
 }
