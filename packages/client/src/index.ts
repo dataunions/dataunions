4299affbe81ddd5bda58f2e2351e9a7b5542ebd6
Tim Oxley <secoif@gmail.com>
2022-05-06 09:32:51 -0400

    feat(client): Add index files. Start working on esbuild web version.

---
+++ packages/client/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * DataUnionClient
+ *
+ * @packageDocumentation
+ * @module DataUnionClient
+ */
+
+import { DataUnionClient } from './DataUnionClient'
+
+export * from './index-exports'
+export default DataUnionClient
+// Note awful export wrappers in index-commonjs.js & index-esm.mjs
