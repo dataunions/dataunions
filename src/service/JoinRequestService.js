59896f0385b2a14d82364d39ee08eabb23efe027
Henri Pihkala <henri@streamr.network>
2022-07-12 17:39:28 +0200

    implement validation in JoinRequestService

--- src/service/JoinRequestService.js
+++ src/service/JoinRequestService.js
@@ -1,3 +1,7 @@
+const { ethers } = require('ethers')
+
+const TOLERANCE_MILLIS = 5 * 60 * 1000 // 5 min
+
 class JoinRequestService {
 	constructor(logger, dataUnionClient, joinDataUnionFunc) {
 		this.logger = logger
@@ -18,6 +22,41 @@ class JoinRequestService {
 				return result
 			})
 	}
+
+
+
+	async validateJoinRequest(signedObject) {
+		const payload = getSignablePayload(signedObject)
+
+		// Check signature
+		let recoveredAddress
+		try {
+			recoveredAddress = ethers.utils.verifyMessage(payload, signedObject.signature)
+		} catch (err) {
+			throw new InvalidSignatureError(`Failed to recover address from signature: ${err}`)
+		}
+
+		if (recoveredAddress.toLowerCase() !== signedObject.member.toLowerCase()) {
+			throw new InvalidSignatureError(`Invalid signature! recoveredAddress: ${recoveredAddress}, member: ${signedObject.member}!`)
+		}
+
+		// Check timestamp
+		const currentTime = new Date()
+		const diff = currentTime.getTime() - new Date(signedObject.timestamp).getTime()
+		if (Math.abs(diff) > TOLERANCE_MILLIS) {
+			throw new InvalidTimestampError(`Timestamp rejected! Request: ${
+				signedObject.timestamp
+			}, current: ${
+				currentTime.toISOString()
+			}, diff (sec): ${
+				diff / 1000
+			}, tolerance (sec): ${
+				TOLERANCE_MILLIS / 1000
+			}`)
+		}
+
+		return true
+	}
 }
 
 function joinDataUnion(dataUnionClient, memberAddress, dataUnionAddres) {
@@ -28,7 +67,46 @@ function joinDataUnion(dataUnionClient, memberAddress, dataUnionAddres) {
 	}))
 }
 
+/**
+ * Sorts the keys of object into alphabetical order, and
+ * creates a concatenated string of all the values.
+ * The key 'signature' is ignored.
+ * 
+ * The method works recursively for nested objects and arrays.
+ * 
+ * This implementation must match the one in the DU client!
+ */
+function getSignablePayload(object) {
+	let result = ""
+
+	if (object == null) {
+		result += 'null'
+	} else if (Array.isArray(object)) {
+		result += JSON.stringify(object.map((item) => getSignablePayload(item)))
+	} else if (typeof object === 'object') {
+		const sortedKeys = Object.keys(object).sort()
+		sortedKeys.forEach((key) => {
+			if (key === 'signature') {
+				return
+			}
+
+			result += key
+			const value = object[key]
+			result += getSignablePayload(value)
+		})
+	} else {
+		result += object
+	}
+
+	return result
+}
+
+class InvalidSignatureError extends Error {}
+class InvalidTimestampError extends Error {}
+
 module.exports = {
 	JoinRequestService,
 	joinDataUnion,
+	InvalidSignatureError,
+	InvalidTimestampError,
 }
--- test/service/JoinRequestService.test.js
+++ test/service/JoinRequestService.test.js
@@ -1,6 +1,9 @@
-const { assert } = require('chai')
+const chai = require('chai')
+const { assert, expect } = chai
+chai.use(require('chai-as-promised'))
+const sinon = require('sinon')
 const pino = require('pino')
-const { JoinRequestService } = require('../../src/service/JoinRequestService')
+const { JoinRequestService, InvalidSignatureError, InvalidTimestampError } = require('../../src/service/JoinRequestService')
 const domain = require('../../src/domain')
 
 describe('Join Request Service', () => {
@@ -86,5 +89,82 @@ describe('Join Request Service', () => {
 		} catch (err) {
 			// Prevent Mocha from catching this expected error
 		}
+	})
+
+	describe('validateJoinRequest', () => {
+		let clock
+
+		beforeEach(() => {
+			clock = sinon.useFakeTimers(new Date('2022-07-01T00:00:00Z').getTime())
+		})
+
+		afterEach(() => {
+			clock.restore()
+		})
+
+		it('accepts valid join requests', async () => {
+			const joinRequest = {
+				member: '0xf79d101E1243cbDdE02d0F49E776fA65de0122ed',
+				dataUnion: DATAUNION_ADDRESS,
+				additionalField: 'is not a problem',
+				timestamp: '2022-07-01T00:00:00Z',
+				// eslint-disable-next-line max-len
+				signature: '0xaabb03e281eb87df25ecf5e8c204854c617b45b54a93de85f241736b246e810903f8f3638664739dc022af83acf444bbc445af35f92c60fbe62d7d5ce71194601c'
+			}
+
+			assert.isTrue(await joinRequestService.validateJoinRequest(joinRequest))
+		})
+
+		it('rejects join requests with invalid signature', async () => {
+			const joinRequest = {
+				member: '0xf79d101E1243cbDdE02d0F49E776fA65de0122ed',
+				dataUnion: DATAUNION_ADDRESS,
+				additionalField: 'is not a problem',
+				timestamp: '2022-07-01T00:00:00Z',
+				// eslint-disable-next-line max-len
+				signature: '0xbbbb03e281eb87df25ecf5e8c204854c617b45b54a93de85f241736b246e810903f8f3638664739dc022af83acf444bbc445af35f92c60fbe62d7d5ce71194601c'
+			}
+
+			await expect(joinRequestService.validateJoinRequest(joinRequest)).to.be.rejectedWith(InvalidSignatureError)
+		})
+
+		it('rejects join requests with tampered data', async () => {
+			const joinRequest = {
+				member: '0xf79d101E1243cbDdE02d0F49E776fA65de0122ed',
+				dataUnion: DATAUNION_ADDRESS,
+				additionalField: 'has been tampered',
+				timestamp: '2022-07-01T00:00:00Z',
+				// eslint-disable-next-line max-len
+				signature: '0xaabb03e281eb87df25ecf5e8c204854c617b45b54a93de85f241736b246e810903f8f3638664739dc022af83acf444bbc445af35f92c60fbe62d7d5ce71194601c'
+			}
+
+			await expect(joinRequestService.validateJoinRequest(joinRequest)).to.be.rejectedWith(InvalidSignatureError)
+		})
+
+		it('rejects join requests in the future', async () => {
+			const joinRequest = {
+				member: '0xf79d101E1243cbDdE02d0F49E776fA65de0122ed',
+				dataUnion: DATAUNION_ADDRESS,
+				additionalField: 'is not a problem',
+				timestamp: '2022-07-01T00:06:00Z',
+				// eslint-disable-next-line max-len
+				signature: '0x698a0e58b60998c2632642e04de0a9586b9d274cdb284955fb6ed7b3406560e458f586c77cff8bee2dfb1c98e667fea8e06a3eafc01f4b60e38771e4d1a73e511c'
+			}
+
+			await expect(joinRequestService.validateJoinRequest(joinRequest)).to.be.rejectedWith(InvalidTimestampError)
+		})
+
+		it('rejects join requests in the past', async () => {
+			const joinRequest = {
+				member: '0xf79d101E1243cbDdE02d0F49E776fA65de0122ed',
+				dataUnion: DATAUNION_ADDRESS,
+				additionalField: 'is not a problem',
+				timestamp: '2022-06-30T23:50:00Z',
+				// eslint-disable-next-line max-len
+				signature: '0x8c0fc7942cd97d41f243f199a3ae88db624475ce438c1bab630dc174c05e29925ef20a1fc66aa0ed0879927f0850511da3adb67328ced0e620b9949eebc5b3681c'
+			}
+
+			await expect(joinRequestService.validateJoinRequest(joinRequest)).to.be.rejectedWith(InvalidTimestampError)
+		})
 	})
 })
