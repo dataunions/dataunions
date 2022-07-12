5204b6f6df657ced812871a10abcad72036ec29e
Henri Pihkala <henri@streamr.network>
2022-07-12 18:47:32 +0200

    clean up JoinRequestService.test.js

--- src/service/JoinRequestService.js
+++ src/service/JoinRequestService.js
@@ -13,17 +13,24 @@ class JoinRequestService {
 	}
 
 	async create(member, dataUnion) {
-		return this.joinDataUnionFunc(this.dataUnionClient, member.toString(), dataUnion.toString())
-			.then(() => {
-				const result = {
-					member: member.toString(),
-					dataUnion: dataUnion.toString(),
-				}
-				return result
-			})
+		let du
+		try {
+			du = await this.dataUnionClient.getDataUnion(dataUnion.toString())
+		} catch (err) {
+			throw new DataUnionRetrievalError(`Error while retrieving data union ${dataUnion}: ${err.message}`)
+		}
+		
+		try {
+			await du.addMembers([member.toString()])
+		} catch (err) {
+			throw new DataUnionJoinError(`Error while adding member ${member} to data union ${dataUnion}: ${err.message}`)
+		}
+
+		return {
+			member: member.toString(),
+			dataUnion: dataUnion.toString(),
+		}
 	}
-
-
 
 	async validateJoinRequest(signedObject) {
 		const payload = getSignablePayload(signedObject)
@@ -57,14 +64,6 @@ class JoinRequestService {
 
 		return true
 	}
-}
-
-function joinDataUnion(dataUnionClient, memberAddress, dataUnionAddres) {
-	return dataUnionClient.getDataUnion(dataUnionAddres).catch((err) => {
-		throw new Error(`Error while retrieving data union: ${err.message}`)
-	}).then((du) => du.addMembers([memberAddress]).catch((err) => {
-		throw new Error(`Error while adding a member to data union: ${err.message}`)
-	}))
 }
 
 /**
@@ -103,10 +102,13 @@ function getSignablePayload(object) {
 
 class InvalidSignatureError extends Error {}
 class InvalidTimestampError extends Error {}
+class DataUnionRetrievalError extends Error {}
+class DataUnionJoinError extends Error {}
 
 module.exports = {
 	JoinRequestService,
-	joinDataUnion,
 	InvalidSignatureError,
 	InvalidTimestampError,
+	DataUnionRetrievalError,
+	DataUnionJoinError,
 }
--- test/service/JoinRequestService.test.js
+++ test/service/JoinRequestService.test.js
@@ -3,13 +3,19 @@ const { assert, expect } = chai
 chai.use(require('chai-as-promised'))
 const sinon = require('sinon')
 const pino = require('pino')
-const { JoinRequestService, InvalidSignatureError, InvalidTimestampError } = require('../../src/service/JoinRequestService')
+const { JoinRequestService, InvalidSignatureError, InvalidTimestampError, DataUnionJoinError, DataUnionRetrievalError } = require('../../src/service/JoinRequestService')
 const domain = require('../../src/domain')
 
 describe('Join Request Service', () => {
 	const MEMBER_ADDRESS = '0x0123456789012345678901234567890123456789'
 	const DATAUNION_ADDRESS = '0x1234567890123456789012345678901234567890'
+
+	const member = new domain.Address(MEMBER_ADDRESS)
+	const dataUnion = new domain.Address(DATAUNION_ADDRESS)
+
 	let joinRequestService
+	let dataUnionClient
+	let dataUnionObject
 	let logger
 
 	before(() => {
@@ -19,76 +25,37 @@ describe('Join Request Service', () => {
 		})
 	})
 
-	after(() => {
+	beforeEach(() => {
+		dataUnionObject = {
+			addMembers: sinon.stub().resolves(true),
+		}
+
+		dataUnionClient = {
+			getDataUnion: sinon.stub().resolves(dataUnionObject),
+		}
+
+		joinRequestService = new JoinRequestService(logger, dataUnionClient)
+	})
+
+	afterEach(() => {
 		joinRequestService = undefined
 	})
 
 	it('handler join request success', async () => {
-		let isJoinDataUnionFuncCalled = false
-		joinRequestService = new JoinRequestService(
-			logger,
-			undefined, // Data Union Client
-			(_dataUnionClient, memberAddress, dataUnionAddress) => {
-				isJoinDataUnionFuncCalled = true
-				return Promise.resolve({
-					member: memberAddress,
-					dataUnion: dataUnionAddress,
-				})
-			}, // Join Data Union Function
-		)
-
-		const member = new domain.Address(MEMBER_ADDRESS)
-		const dataUnion = new domain.Address(DATAUNION_ADDRESS)
 		const response = await joinRequestService.create(member, dataUnion)
-		assert.isTrue(isJoinDataUnionFuncCalled)
+		assert.isTrue(dataUnionObject.addMembers.calledWith([MEMBER_ADDRESS]))
 		assert.equal(response.member, MEMBER_ADDRESS)
 		assert.equal(response.dataUnion, DATAUNION_ADDRESS)
 	})
 
 	it('handle join request error when data union is not found', async () => {
-		let isJoinDataUnionFuncCalled = false
-		joinRequestService = new JoinRequestService(
-			logger,
-			undefined, // Streamr Client
-			(_dataUnionClient, _memberAddress, dataUnionAddress) => {
-				isJoinDataUnionFuncCalled = true
-				const msg = `Error while adding a member to data union: ${dataUnionAddress} is not a Data Union!`
-				return Promise.reject(new Error(msg))
-			}, // Join Data Union Function
-		)
-
-		const member = new domain.Address(MEMBER_ADDRESS)
-		const dataUnion = new domain.Address(DATAUNION_ADDRESS)
-		try {
-			const response = await joinRequestService.create(member, dataUnion)
-			console.log(response)
-			assert.isTrue(isJoinDataUnionFuncCalled)
-		} catch (err) {
-			// Prevent Mocha from catching this expected error
-		}
+		dataUnionClient.getDataUnion = sinon.stub().rejects()
+		await expect(joinRequestService.create(member, dataUnion)).to.be.rejectedWith(DataUnionRetrievalError)
 	})
 
 	it('handle join request error when joining data union fails', async () => {
-		let isJoinDataUnionFuncCalled = false
-		joinRequestService = new JoinRequestService(
-			logger,
-			undefined, // Streamr Client
-			(_dataUnionClient, _memberAddress, _dataUnionAddress) => {
-				isJoinDataUnionFuncCalled = true
-				const msg = `Error while adding a member to data union: Error while joining Data Union`
-				return Promise.reject(new Error(msg))
-			}, // Join Data Union Function
-		)
-
-		const member = new domain.Address(MEMBER_ADDRESS)
-		const dataUnion = new domain.Address(DATAUNION_ADDRESS)
-		try {
-			const response = await joinRequestService.create(member, dataUnion)
-			console.log(response)
-			assert.isTrue(isJoinDataUnionFuncCalled)
-		} catch (err) {
-			// Prevent Mocha from catching this expected error
-		}
+		dataUnionObject.addMembers = sinon.stub().rejects()
+		await expect(joinRequestService.create(member, dataUnion)).to.be.rejectedWith(DataUnionJoinError)
 	})
 
 	describe('validateJoinRequest', () => {
