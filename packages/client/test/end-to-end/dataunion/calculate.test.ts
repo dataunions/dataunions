2244ea10cc735345303768abfc2f383fd8421072
Juuso Takalainen <juuso@streamr.network>
2022-07-18 21:10:02 +0300

    test: remove pointless test
    I wonder where the useful test went...

--- packages/client/test/end-to-end/dataunion/calculate.test.ts
+++ packages/client/test/end-to-end/dataunion/calculate.test.ts
@@ -1,14 +1,3 @@
-import { expectInvalidAddress, getRandomClient } from '../../test-utils/utils'
-
-// This test will fail when new docker images are pushed with updated DU smart contracts
-// -> generate new codehashes for calculateDataUnionMainnetAddress() and calculateDataUnionSidechainAddress()
-
+// TODO: add the calculate test: calculate address, then deploy, expect calculated address be correct
 describe('DataUnion calculate', () => {
-    it('getDataUnion fails for bad addresses', async () => {
-        const client = getRandomClient()
-        await expectInvalidAddress(async () => client.getDataUnion('invalid-address'))
-        await expect(client.getDataUnion('0x2222222222222222222222222222222222222222'))
-            .rejects
-            .toThrow('0x2222222222222222222222222222222222222222 is not a Data Union!')
-    })
 })
