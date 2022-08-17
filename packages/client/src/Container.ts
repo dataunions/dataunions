d5b3f9c84413b59ded7b87223090f54c61612800
Kare Nuorteva <kare.nuorteva@me.com>
2022-08-17 14:10:03 +0300

    refactor: remove Container.ts

--- packages/client/src/Container.ts
+++
@@ -1,6 +0,0 @@
-/**
- * DI Token for injecting the Client container.
- * Use sparingly, but can be necessary for factories
- * or to work around circular dependencies.
- */
-export const DataUnionContainer = Symbol('DataUnionContainer')
