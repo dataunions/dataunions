23e4d2ae39502e3cc86b14d6cab009c686258915
Juuso Takalainen <juuso@streamr.network>
2022-08-26 12:46:01 +0300

    test: improved test as suggested by PR review

--- packages/contracts/test/contracts/unichain/DataUnionTemplate.test.ts
+++ packages/contracts/test/contracts/unichain/DataUnionTemplate.test.ts
@@ -7,11 +7,10 @@ const log = Debug("Streamr:du:test:DataUnionTemplate")
 //const log = console.log  // for debugging?
 
 import DataUnionTemplateJson from "../../../artifacts/contracts/unichain/DataUnionTemplate.sol/DataUnionTemplate.json"
-
 import TestTokenJson from "../../../artifacts/contracts/test/TestToken.sol/TestToken.json"
 import FeeOracleJson from "../../../artifacts/contracts/DefaultFeeOracle.sol/DefaultFeeOracle.json"
 
-import type { DataUnionTemplate, TestToken } from "../../../typechain"
+import type { DataUnionTemplate, TestToken, DefaultFeeOracle } from "../../../typechain"
 
 type EthereumAddress = string
 
@@ -49,8 +48,8 @@ async function getWithdrawSignature(
 
 describe("DataUnionTemplate", () => {
     const accounts = provider.getWallets()
-    const creator = accounts[0]
-    const duBeneficiary = accounts[1]
+    const dao = accounts[0]
+    const admin = accounts[1]
     const agents = accounts.slice(2, 4)
     const members = accounts.slice(4, 7)
     const others = accounts.slice(7)
@@ -60,28 +59,27 @@ describe("DataUnionTemplate", () => {
     const o = others.map(outsider => outsider.address)
 
     let testToken: TestToken
-    let feeOracle: Contract
+    let feeOracle: DefaultFeeOracle
     let dataUnionSidechain: DataUnionTemplate
     let dataUnionSidechainAgent: DataUnionTemplate
     let dataUnionSidechainMember0: DataUnionTemplate
 
     before(async () => {
-        testToken = await deployContract(creator, TestTokenJson, ["name", "symbol"]) as TestToken
-        await testToken.mint(creator.address, parseEther("10000"))
-        feeOracle = await deployContract(creator, FeeOracleJson, [parseEther("0.1")])
+        testToken = await deployContract(dao, TestTokenJson, ["name", "symbol"]) as TestToken
+        await testToken.mint(dao.address, parseEther("100000"))
+        feeOracle = await deployContract(dao, FeeOracleJson, [parseEther("0.01")]) as DefaultFeeOracle
 
         log("List of relevant addresses:")
-        log("  testToken: ", testToken.address)
-        log("  creator: ", creator.address)
+        log("  testToken: %s", testToken.address)
+        log("  dao: %s", dao.address)
+        log("  admin: %s", admin.address)
         log("  agents: %o", a)
         log("  members: %o", m)
-        log("  outsider addresses used in tests: %o", others.map(x => x.address))
+        log("  outsider addresses used in tests: %o", o)
     })
 
     beforeEach(async () => {
-        const dummyAddress = a[0]
-
-        dataUnionSidechain = await deployContract(creator, DataUnionTemplateJson, []) as DataUnionTemplate
+        dataUnionSidechain = await deployContract(admin, DataUnionTemplateJson, []) as DataUnionTemplate
         dataUnionSidechainAgent = dataUnionSidechain.connect(agents[1])
         dataUnionSidechainMember0 = dataUnionSidechain.connect(members[0])
 
@@ -95,12 +93,12 @@ describe("DataUnionTemplate", () => {
         //     address protocolFeeOracleAddress
         // )
         await dataUnionSidechain.initialize(
-            creator.address,
+            admin.address,
             testToken.address,
             a,
             "1",
-            parseEther("0.1"),
-            duBeneficiary.address,
+            parseEther("0.09"), // total fees are 1% + 9% = 10%
+            dao.address,
             feeOracle.address,
         )
         await dataUnionSidechainAgent.addMembers(m)
@@ -119,37 +117,44 @@ describe("DataUnionTemplate", () => {
         // repeating it should do nothing (also not throw)
         await dataUnionSidechain.connect(randomOutsider).refreshRevenue()
 
-        expect(await dataUnionSidechain.totalEarnings()).to.equal(2400)
-        expect(await dataUnionSidechain.totalAdminFees()).to.equal(300)
-        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(300)
-        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(800)
-        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(800)
-        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(800)
+        expect(await dataUnionSidechain.totalEarnings()).to.equal(2700)
+        expect(await dataUnionSidechain.totalAdminFees()).to.equal(270)
+        expect(await dataUnionSidechain.getEarnings(admin.address)).to.equal(270)
+        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(30)
+        expect(await dataUnionSidechain.getEarnings(dao.address)).to.equal(30)
+        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(900)
 
         // drop a member, send more tokens, check accounting
         await expect(dataUnionSidechainAgent.partMember(m[0])).to.emit(dataUnionSidechain, "MemberParted")
-        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(800)
+        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(900)
         await testToken.transfer(dataUnionSidechain.address, "2000")
         await dataUnionSidechain.connect(randomOutsider).refreshRevenue()
-        expect(await dataUnionSidechain.totalEarnings()).to.equal(4000)
-        expect(await dataUnionSidechain.totalAdminFees()).to.equal(500)
-        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(500)
-        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(800)
-        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(1600)
-        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(1600)
+        expect(await dataUnionSidechain.totalEarnings()).to.equal(4500)
+        expect(await dataUnionSidechain.totalAdminFees()).to.equal(450)
+        expect(await dataUnionSidechain.getEarnings(admin.address)).to.equal(450)
+        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(50)
+        expect(await dataUnionSidechain.getEarnings(dao.address)).to.equal(50)
+        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(1800)
+        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(1800)
         await expect(dataUnionSidechainAgent.addMember(m[0])).to.emit(dataUnionSidechain, "MemberJoined")
 
         // add a member, send tokens, check accounting
         await expect(dataUnionSidechainAgent.addMember(newMember.address)).to.emit(dataUnionSidechain, "MemberJoined")
         await testToken.transfer(dataUnionSidechain.address, "4000")
         await dataUnionSidechain.connect(randomOutsider).refreshRevenue()
-        expect(await dataUnionSidechain.totalEarnings()).to.equal(7200)
-        expect(await dataUnionSidechain.totalAdminFees()).to.equal(900)
-        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(900)
-        expect(await dataUnionSidechain.getEarnings(newMember.address)).to.equal(800)
-        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(1600)
-        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(2400)
-        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(2400)
+        expect(await dataUnionSidechain.totalEarnings()).to.equal(8100)
+        expect(await dataUnionSidechain.totalAdminFees()).to.equal(810)
+        expect(await dataUnionSidechain.getEarnings(admin.address)).to.equal(810)
+        expect(await dataUnionSidechain.totalProtocolFees()).to.equal(90)
+        expect(await dataUnionSidechain.getEarnings(dao.address)).to.equal(90)
+        expect(await dataUnionSidechain.getEarnings(newMember.address)).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(1800)
+        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(2700)
+        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(2700)
         await expect(dataUnionSidechainAgent.partMember(newMember.address)).to.emit(dataUnionSidechain, "MemberParted")
     })
 
@@ -206,15 +211,17 @@ describe("DataUnionTemplate", () => {
     it("getEarnings", async () => {
         await expect(dataUnionSidechain.getEarnings(o[0])).to.be.revertedWith("error_notMember")
         await expect(dataUnionSidechain.getEarnings(a[0])).to.be.revertedWith("error_notMember")
-        await expect(dataUnionSidechain.getEarnings(creator.address)).to.be.revertedWith("error_notMember")
+        await expect(dataUnionSidechain.getEarnings(admin.address)).to.be.revertedWith("error_notMember")
         expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(0)
 
         await testToken.transfer(dataUnionSidechain.address, "3000")
         await dataUnionSidechain.refreshRevenue()
 
-        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(800)
-        expect(await dataUnionSidechain.getEarnings(creator.address)).to.equal(300)
-        expect(await dataUnionSidechain.getEarnings(duBeneficiary.address)).to.equal(300)
+        expect(await dataUnionSidechain.getEarnings(m[0])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[1])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(m[2])).to.equal(900)
+        expect(await dataUnionSidechain.getEarnings(admin.address)).to.equal(270)
+        expect(await dataUnionSidechain.getEarnings(dao.address)).to.equal(30)
     })
 
     async function getBalances(addresses: EthereumAddress[]) {
@@ -232,7 +239,7 @@ describe("DataUnionTemplate", () => {
         await testToken.transfer(dataUnionSidechain.address, "3000")
         await dataUnionSidechain.refreshRevenue()
         await expect(dataUnionSidechain.withdrawMembers(m, false)).to.emit(dataUnionSidechain, "EarningsWithdrawn")
-        expect(await getBalanceIncrements(m, balances)).to.deep.equal([ 800, 800, 800 ])
+        expect(await getBalanceIncrements(m, balances)).to.deep.equal([ 900, 900, 900 ])
     })
 
     it("withdrawAll", async () => {
@@ -243,7 +250,7 @@ describe("DataUnionTemplate", () => {
         await expect(dataUnionSidechainMember0.withdrawAll(m[0], false)).to.emit(dataUnionSidechain, "EarningsWithdrawn")
         await expect(dataUnionSidechain.withdrawAll(m[1], false)).to.emit(dataUnionSidechain, "EarningsWithdrawn")
         await dataUnionSidechain.withdrawAll(m[1], false)    // this should do nothing, also not revert
-        expect(await getBalanceIncrements(m, balances)).to.deep.equal([ 800, 800, 0 ])
+        expect(await getBalanceIncrements(m, balances)).to.deep.equal([ 900, 900, 0 ])
     })
 
     it("withdrawAllTo", async () => {
@@ -255,7 +262,7 @@ describe("DataUnionTemplate", () => {
         const after = await testToken.balanceOf(o[0])
 
         const diff = after.sub(before)
-        expect(diff).to.equal(800)
+        expect(diff).to.equal(900)
     })
 
     it("withdrawToSigned", async () => {
@@ -292,13 +299,13 @@ describe("DataUnionTemplate", () => {
         await expect(dataUnionSidechainRecipient.withdrawAllToSigned(m[0], r, false, signature)).to.be.revertedWith("error_badSignature")
         await expect(dataUnionSidechainRecipient.withdrawAllToSigned(m[1], r, false, signature)).to.emit(dataUnionSidechain, "EarningsWithdrawn")
 
-        expect(await testToken.balanceOf(r)).to.equal(800)
+        expect(await testToken.balanceOf(r)).to.equal(900)
     })
 
     it("transferToMemberInContract", async () => {
         await testToken.approve(dataUnionSidechain.address, "2000")
-        await dataUnionSidechain.transferToMemberInContract(o[0], "1000")
-        await dataUnionSidechain.transferToMemberInContract(m[0], "1000")
+        await dataUnionSidechain.connect(dao).transferToMemberInContract(o[0], "1000")
+        await dataUnionSidechain.connect(dao).transferToMemberInContract(m[0], "1000")
         expect(await dataUnionSidechain.getWithdrawableEarnings(o[0])).to.equal(1000)
         expect(await dataUnionSidechain.getWithdrawableEarnings(m[0])).to.equal(1000)
 
@@ -324,15 +331,15 @@ describe("DataUnionTemplate", () => {
         // await expect(dataUnionSidechain.transferWithinContract(m[1], "100")).to.be.revertedWith("error_notMember")
         await expect(dataUnionSidechainMember0.transferWithinContract(m[1], "100")).to.emit(dataUnionSidechain, "TransferWithinContract")
         await expect(dataUnionSidechainMember0.transferWithinContract(o[1], "100")).to.emit(dataUnionSidechain, "TransferWithinContract")
-        expect(await dataUnionSidechain.getWithdrawableEarnings(m[0])).to.equal(600)
-        expect(await dataUnionSidechain.getWithdrawableEarnings(m[1])).to.equal(900)
-        expect(await dataUnionSidechain.getWithdrawableEarnings(m[2])).to.equal(800)
+        expect(await dataUnionSidechain.getWithdrawableEarnings(m[0])).to.equal(700)  // = 900 - 100 - 100
+        expect(await dataUnionSidechain.getWithdrawableEarnings(m[1])).to.equal(1000) // = 900 + 100
+        expect(await dataUnionSidechain.getWithdrawableEarnings(m[2])).to.equal(900)  // no changes
         expect(await dataUnionSidechain.getWithdrawableEarnings(o[1])).to.equal(100)
         // those who received some in-contract balance but aren't members should be marked inactive by _increaseBalance
         expect(await dataUnionSidechain.inactiveMemberCount()).to.equal(3)
         expect((await dataUnionSidechain.memberData(o[1])).status).to.equal(2)
-        expect((await dataUnionSidechain.memberData(duBeneficiary.address)).status).to.equal(2)
-        expect((await dataUnionSidechain.memberData(creator.address)).status).to.equal(2)
+        expect((await dataUnionSidechain.memberData(dao.address)).status).to.equal(2)
+        expect((await dataUnionSidechain.memberData(admin.address)).status).to.equal(2)
     })
 
     it("getStats", async () => {
@@ -352,13 +359,13 @@ describe("DataUnionTemplate", () => {
             joinPartAgentCount
         ] = await dataUnionSidechain.getStats()
         expect(totalRevenue).to.equal(3000)
-        expect(totalEarnings).to.equal(2400)
-        expect(totalAdminFees).to.equal(300)
-        expect(totalProtocolFees).to.equal(300)
+        expect(totalEarnings).to.equal(2700)
+        expect(totalAdminFees).to.equal(270)
+        expect(totalProtocolFees).to.equal(30)
         expect(totalEarningsWithdrawn).to.equal(500)
         expect(activeMemberCount).to.equal(3)
-        expect(inactiveMemberCount).to.equal(0) // creator and duBeneficiary are cleaned out of this number though they show up in the "inactiveMemberCount"
-        expect(lifetimeMemberEarnings).to.equal(800)
+        expect(inactiveMemberCount).to.equal(0) // admin and dao are cleaned out of this number though they show up in the "inactiveMemberCount"
+        expect(lifetimeMemberEarnings).to.equal(900)
         expect(joinPartAgentCount).to.equal(2)
     })
 
@@ -382,12 +389,12 @@ describe("DataUnionTemplate", () => {
     it("fails to initialize twice", async () => {
         const a = agents.map(agent => agent.address)
         await expect(dataUnionSidechain.initialize(
-            creator.address,
+            admin.address,
             testToken.address,
             a,
             "1",
             parseEther("0.1"),
-            duBeneficiary.address,
+            dao.address,
             feeOracle.address,
         )).to.be.revertedWith("error_alreadyInitialized")
     })
@@ -420,19 +427,28 @@ describe("DataUnionTemplate", () => {
         expect(await dataUnionSidechain.owner()).to.equal(o[0])
 
         await expect(dataUnionSidechain.transferOwnership(o[0])).to.be.revertedWith("error_onlyOwner")
-        await dataUnionSidechain.connect(others[0]).transferOwnership(creator.address)
+        await dataUnionSidechain.connect(others[0]).transferOwnership(admin.address)
         await expect(dataUnionSidechain.claimOwnership()).to.emit(dataUnionSidechain, "OwnershipTransferred")
-        expect(await dataUnionSidechain.owner()).to.equal(creator.address)
+        expect(await dataUnionSidechain.owner()).to.equal(admin.address)
     })
 
     it("rejects unexpected ERC677 tokens", async () => {
-        const randomToken = await deployContract(creator, TestTokenJson, ["random", "RND"]) as TestToken
-        await randomToken.mint(creator.address, parseEther("10000"))
+        const randomToken = await deployContract(admin, TestTokenJson, ["random", "RND"]) as TestToken
+        await randomToken.mint(admin.address, parseEther("10000"))
         await expect(randomToken.transferAndCall(dataUnionSidechain.address, "1000", "0x")).to.be.revertedWith("error_onlyTokenContract")
     })
 
-    it("rejects fees that sum above 1.0", async () => {
-        await expect(dataUnionSidechain.setAdminFee(parseEther("0.95"))).to.be.revertedWith("error_adminFee")
+    it("rejects admin fee that would cause total fees sum above 1.0", async () => {
+        await expect(dataUnionSidechain.setAdminFee(parseEther("0.995"))).to.be.revertedWith("error_adminFee")
+    })
+
+    it("adjusts an admin fee that would cause total fees sum above 1.0", async () => {
+        await expect(dataUnionSidechain.setAdminFee(parseEther("0.9"))).to.emit(dataUnionSidechain, "AdminFeeChanged")
+        expect(await dataUnionSidechain.adminFeeFraction()).to.equal(parseEther("0.9"))
+        await feeOracle.setFee(parseEther("0.2"))
+        assert(await testToken.transfer(dataUnionSidechain.address, "3000"))
+        await dataUnionSidechain.refreshRevenue()
+        expect(await dataUnionSidechain.adminFeeFraction()).to.equal(parseEther("0.8"))
     })
 
     it("cannot swap modules after locking", async () => {
