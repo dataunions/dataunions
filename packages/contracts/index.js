9d45a5e7e05d6864da7620171a89203686665e67
jtakalai <juuso@streamr.network>
2022-09-08 12:05:30 +0300

    Make DefaultFeeOracle and DataUnionFactory upgradeable (#339)
    * feat: DefaultFeeOracle and DataUnionFactory are now upgradeable
    
    * build: add hardhat-upgrades and contracts-upgradeable as deps
    
    * fix: remove prepare step
    
    it's super annoying, and now it's breaking CI. No idea why it was there in the first place.
    
    * fix: lint
    
    * build: add missing devDependency
    
    * test: fix deployment of upgradeable proxies
    
    * feat: upgrade openzeppelin dependency
    
    * fix: upgradeable init parent calls
    
    * test: fix upgradeable initialize
    
    * fix: attempt to fix makefile
    
    it's probably way too long and complex to fix. This fix is clear: contracts were moved from deps to devDeps. But it's too complex to patch in the contracts-upgradeable, too, I'm afraid...
    
    * build: remove solidity makefile build step
    
    it's not used anywhere, and it's blocking the CI. I'll make a ticket of fixing it.
    
    * fix: tabs to spaces
    
    * refactor: more readable variable names
    
    * build: harmonize hardhat and @streamr/config network names
    
    * test: better naming
    
    * feat: ETH-358 deployment script + artifacts
    
    artifacts are needed for upgrading the proxy implementations later

--- packages/contracts/index.js
+++ packages/contracts/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { abi: tAbi, bytecode: tBytecode } = require("./artifacts/contracts/unichain/DataUnionTemplate.sol/DataUnionTemplate.json")
-const { abi: fAbi, bytecode: fBytecode } = require("./artifacts/contracts/unichain/DataUnionFactory.sol/DataUnionFactory.json")
-const { abi: oAbi, bytecode: oBytecode } = require("./artifacts/contracts/DefaultFeeOracle.sol/DefaultFeeOracle.json")
+const { abi: templateAbi, bytecode: templateBytecode } = require("./artifacts/contracts/unichain/DataUnionTemplate.sol/DataUnionTemplate.json")
+const { abi: factoryAbi, bytecode: factoryBytecode } = require("./artifacts/contracts/unichain/DataUnionFactory.sol/DataUnionFactory.json")
+const { abi: oracleAbi, bytecode: oracleBytecode } = require("./artifacts/contracts/DefaultFeeOracle.sol/DefaultFeeOracle.json")
 
 module.exports = {
-    dataUnionTemplate: { abi: tAbi, bytecode: tBytecode },
-    dataUnionFactory: { abi: fAbi, bytecode: fBytecode },
-    defaultFeeOracle: { abi: oAbi, bytecode: oBytecode },
+    dataUnionTemplate: { abi: templateAbi, bytecode: templateBytecode },
+    dataUnionFactory: { abi: factoryAbi, bytecode: factoryBytecode },
+    defaultFeeOracle: { abi: oracleAbi, bytecode: oracleBytecode },
 }
