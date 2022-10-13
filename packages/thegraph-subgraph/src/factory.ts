12977417ae65ba1d05390147c1fcee5321f33ca5
Juuso Takalainen <juuso@streamr.network>
2022-10-13 10:56:49 +0300

    feat: support old DU factories (Swash) in the subgraph

--- packages/thegraph-subgraph/src/factory.ts
+++ packages/thegraph-subgraph/src/factory.ts
@@ -1,17 +1,28 @@
-import { log, BigInt } from '@graphprotocol/graph-ts'
+import { log, BigInt, Address } from '@graphprotocol/graph-ts'
 
 import { DUCreated } from '../generated/DataUnionFactory/DataUnionFactory'
+import { SidechainDUCreated } from '../generated/DataUnionFactorySidechain/DataUnionFactorySidechain'
 import { DataUnion as DataUnionDatabaseObject } from '../generated/schema'
 import { DataUnion } from '../generated/templates'
+
+// event SidechainDUCreated(address indexed mainnet, address indexed sidenet, address indexed owner, address template);
+export function handleDU2Created(event: SidechainDUCreated): void {
+    let duAddress = event.params.mainnet
+    log.warning('[old] handleDU2Created: address={} blockNumber={}', [duAddress.toHexString(), event.block.number.toString()])
+    createDataUnion(duAddress, event.block.timestamp)
+}
 
 export function handleDUCreated(event: DUCreated): void {
     let duAddress = event.params.du
     log.warning('handleDUCreated: address={} blockNumber={}', [duAddress.toHexString(), event.block.number.toString()])
+    createDataUnion(duAddress, event.block.timestamp)
+}
 
+export function createDataUnion(duAddress: Address, creationDate: BigInt): void {
     let dataUnion = new DataUnionDatabaseObject(duAddress.toHexString())
     dataUnion.memberCount = 0
     dataUnion.revenueWei = BigInt.zero()
-    dataUnion.creationDate = event.block.timestamp
+    dataUnion.creationDate = creationDate
     dataUnion.save()
 
     // Instantiate a template: start listening to the new DU contract, trigger src/dataunion.ts on events
