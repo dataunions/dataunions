beef5cf4c029c632310216df4e312b33edd66da6
Juuso Takalainen <juuso@streamr.network>
2022-12-16 16:15:44 +0200

    build: deploy script for updating the contract template

---
+++ packages/contracts/scripts/unichain/update-template.ts
@@ -0,0 +1,39 @@
+import { ContractFactory } from "ethers"
+import { ethers } from "hardhat"
+import { Chains } from "@streamr/config"
+
+import { DataUnionFactory, DataUnionTemplate } from "../../typechain"
+
+const { CHAIN } = process.env
+if (!CHAIN) { throw new Error("Please specify CHAIN environment variable (dev0, dev1, gnosis, polygon, mainnet)") }
+
+const {
+    contracts: {
+        DataUnionFactory: FACTORY_ADDRESS
+    }
+} = Chains.load()[CHAIN]
+
+async function main() {
+    const dataUnionTemplateFactory = await ethers.getContractFactory("DataUnionTemplate")
+    const dataUnionTemplate = await dataUnionTemplateFactory.deploy()
+    await dataUnionTemplate.deployed() as DataUnionTemplate
+    console.log("DU template deployed at %s", dataUnionTemplate.address)
+
+    const factoryFactory = await ethers.getContractFactory("DataUnionFactory") as ContractFactory
+    const factory = factoryFactory.attach(FACTORY_ADDRESS) as DataUnionFactory
+    console.log("DU factory deployed at %s", factory.address)
+
+    const oldTemplateAddress = await factory.dataUnionTemplate()
+    console.log("Old DU template at %s", oldTemplateAddress)
+
+    const tx = await factory.setTemplate(dataUnionTemplate.address)
+    await tx.wait()
+    console.log("DU template updated")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
