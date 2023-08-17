f5762350fe3f118aec1e444285d5b1684e692c3b
Juuso Takalainen <juuso@streamr.network>
2023-08-17 16:04:40 +0300

    build: create subgraph deployment docker image for the fastchain
    ...instead of the old dev-docker slow chain

--- packages/thegraph-subgraph/start.sh
+++ packages/thegraph-subgraph/start.sh
@@ -2,7 +2,10 @@ CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup, waiting 30sec, then deploying subgraph --"
-    sleep 30s; npm run doAll-inside-docker-dev
+    sleep 30s;
+    echo "CREATING THE SUBGRAPH DEPLOYER IMAGE FOR THE   FAST CHAIN"
+    npm run create-fastchain
+    npm run deploy-fastchain
 else
     echo "-- Not first container startup, doing nothing.--"
 fi
