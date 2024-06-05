8a57b6a28231650f7c9385e74736bcd63d57008e
Juuso Takalainen <juuso@streamr.network>
2024-06-05 14:59:47 +0300

    build: update @streamr/config version
    also harmonize @dataunions/client versions because then @streamr/config would also exist in only one version

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
 import "@openzeppelin/hardhat-upgrades"
 import "hardhat-typechain"
 import "solidity-coverage"
@@ -38,6 +39,11 @@ const config: HardhatUserConfig = {
             url: "https://polygon-rpc.com",
             accounts: [process.env.KEY || "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"] // dummy key
         },
+        polygonAmoy: {
+            chainId: 80002,
+            url: process.env.ETHEREUM_RPC || "https://rpc-amoy.polygon.technology",
+            accounts: [process.env.KEY || "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"] // dummy key
+        },
         ethereum: {
             chainId: 1,
             url: "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY || "",
@@ -47,6 +53,22 @@ const config: HardhatUserConfig = {
     typechain: {
         outDir: "typechain",
         target: "ethers-v5"
-    }
+    },
+    etherscan: {
+        apiKey: {
+            polygon: process.env.ETHERSCAN_KEY || "",
+            polygonMumbai: process.env.ETHERSCAN_KEY || "",
+            polygonAmoy: process.env.ETHERSCAN_KEY || "",
+            peaq: process.env.ETHERSCAN_KEY || "",
+        },
+        customChains: [{
+            network: "polygonAmoy",
+            chainId: 80002,
+            urls: {
+                apiURL: "https://api-amoy.polygonscan.com/api",
+                browserURL: "https://amoy.polygonscan.com"
+            },
+        }]
+    },
 }
 export default config
